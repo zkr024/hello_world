29c9d6e5abb0eaec7e5adcd776b81f19ebead212
Oscar <zkr.bm.02403@gmail.com>
2022-08-17 19:10:04 -0500

    messages

--- app/javascript/react/src/components/GreetDetail.jsx
+++ app/javascript/react/src/components/GreetDetail.jsx
@@ -3,7 +3,9 @@ import * as React from 'react'
 const GreetDetail = (props) => {
   return (
     <div>
-      { props.message.greet }
+      <h2>
+        { props.message.greet }
+      </h2>
     </div>
   )
 }
